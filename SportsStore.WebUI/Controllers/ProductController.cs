62f59f235d06d2d564d2e1ab83b310d646e8a60b
Formeo <formeo@yandex.ru>
2017-07-04 23:40:39 +0500

    start develop

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productsRepository) {
+            repository = productsRepository;
+
+        }
+
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}
